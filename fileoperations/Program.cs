4655bdcdb3516d6ad40e23c14ebfd0bb0a0fa508
maddepalli sivani <maddepallisivani@outlook.com>
2022-05-26 10:29:24 +0530

    Add project files.

---
+++ fileoperations/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace fileoperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileDemo();
+        }
+        static void FileDemo()
+        {
+            String dir = Directory.GetCurrentDirectory();
+            String filename = dir + "\\data.txt";
+            if (File.Exists(filename))
+            {
+                Console.WriteLine(filename + " exists");
+            }
+            else
+            {
+                Console.WriteLine(filename + " does not exist");
+                using (StreamWriter wrt = File.CreateText(filename))
+                {
+                    wrt.WriteLine("student name= m.sivani");
+                    wrt.WriteLine("class=10");
+                    wrt.WriteLine("percentage=99");
+                    wrt.WriteLine("rollno=12433");
+                }
+                Console.WriteLine(filename + " created");
+            }
+            string text = File.ReadAllText(filename);
+            Console.WriteLine(text);
+        }
+    }
+}
